fe3f608395df751d663be78fcd95cc56ee92f037
gabrySuerz <tsam028@tecnicosuperiorekennedy.it>
2017-07-17 19:51:22 +0200

    ssl project

---
+++ SSL-CA_30-06-2017/app/src/main/java/com/gabrysuerz/ssl_ca_30_06_2017/MainActivity.java
@@ -0,0 +1,62 @@
+package com.gabrysuerz.ssl_ca_30_06_2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Load CAs from an InputStream
+        // (could be from a resource or ByteArrayInputStream or ...)
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        // From https://www.washington.edu/itconnect/security/ca/load-der.crt
+        InputStream caInput = new BufferedInputStream(new FileInputStream("rootCA.pem"));
+        Certificate ca;
+        try {
+            ca = cf.generateCertificate(caInput);
+            System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+        } finally {
+            caInput.close();
+        }
+
+        // Create a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        // Create a TrustManager that trusts the CAs in our KeyStore
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        // Create an SSLContext that uses our TrustManager
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, tmf.getTrustManagers(), null);
+
+        // Tell the URLConnection to use a SocketFactory from our SSLContext
+        URL url = new URL("https://certs.cac.washington.edu/CAtest/");
+        HttpsURLConnection urlConnection =
+                (HttpsURLConnection) url.openConnection();
+        urlConnection.setSSLSocketFactory(context.getSocketFactory());
+        InputStream in = urlConnection.getInputStream();
+        copyInputStreamToOutputStream(in, System.out);
+    }
+}
